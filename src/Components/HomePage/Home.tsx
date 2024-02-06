98fad798bfdc328a277b5a64641967030c2edcc6
fnguyenn <fnguyen3@ucsc.edu>
2024-02-06 15:59:04 -0800

    Added basic axios call

--- src/Components/HomePage/Home.tsx
+++ src/Components/HomePage/Home.tsx
@@ -1,24 +1,32 @@
 import React, {useState} from "react";
+import axios from "axios";
 import Search from "./Components/Search";
 import "./Components/home.css";
 
 function Home(){
     const [searchInput, setSearchInput] = useState<{ address: string; chain: string } | null>(null);
+    const [data, setData] = useState<any>(null);
 
-    const handleSearchSubmit = (address: string, chain: string) => {
+    const handleSearchSubmit = async (address: string, chain: string) => {
         console.log("Address ", address);
         console.log("Chain: ", chain);
         setSearchInput({ address, chain });
+        try{
+            const response = await axios.get(`http://localhost:8888/active-chains/?address=${address}`);
+                setData(response);
+                console.log(data)
+        } catch(err) {
+                console.error('Error:', err);
+        }
     };
     return(
         <div className="app-container">
             <div className="center-content">
             <h1>Blockchain Analytics</h1>
             <Search onSubmit={handleSearchSubmit}/>
-            {searchInput && (
+            {data && (
                 <div>
-                    <p>User Address: {searchInput.address}</p>
-                    <p>Chain: {searchInput.chain}</p>
+                    {data}
                 </div>
             )}
             </div>
