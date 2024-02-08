08096ac81d5db75b3e4c743d152433850c3f61c2
fnguyenn <fnguyen3@ucsc.edu>
2024-02-08 01:05:24 -0800

    Connected to backend and implemented basic functions

--- src/Services/Services.tsx
+++ src/Services/Services.tsx
@@ -14,3 +14,44 @@ export async function sActiveChains(address: string){
         return 0;
     }
 }
+
+export async function sSimplifiedActiveChains(address: string){
+    try{
+        const response = await axios.get(`${HOST}/active-chains-simplified/${address}`);
+        return response.data.chains;
+    }catch(error){
+        console.error('Error:', error);
+        return 0;
+    }
+}
+
+export async function sNativeBalance(address: string){
+    try{
+        const response = await axios.get(`${HOST}/native-balance/${address}`);
+        return response.data.balance;
+    }catch(error){
+        console.error('Error:', error);
+        return 0;
+    }
+}
+
+export async function sErcBalance(address: string){
+    try{
+        const response = await axios.get(`${HOST}/erc-balance/${address}`);
+        return response.data.tokens;
+    }catch(error){
+        console.error('Error:', error);
+        return 0;
+    }
+}
+
+export async function sNFT(address: string){
+    try{
+        const response = await axios.get(`${HOST}/nft/${address}`);
+        console.log(response);
+        return response.data.nfts;
+    }catch(error){
+        console.error('Error:', error);
+        return 0;
+    }
+}
