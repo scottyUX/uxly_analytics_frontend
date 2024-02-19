0f85c051e4a9877b4f7cfa0764fe08951b9b8132
fnguyenn <fnguyen3@ucsc.edu>
2024-02-18 19:40:39 -0800

    Made making window smaller bearable

--- src/Components/HomePage/SearchComponents/DisplayWallet/TruncateText.tsx
+++ src/Components/HomePage/SearchComponents/DisplayWallet/TruncateText.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./displaywallet.css";
 
 interface TruncatedTextProps {
@@ -8,6 +8,26 @@ interface TruncatedTextProps {
 
 function TruncatedText({ text, maxLength }: TruncatedTextProps): JSX.Element {
   const [isClicked, setIsClicked] = useState(false);
+  const [max, setMax] = useState<number>(maxLength);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 950) {
+        setIsClicked(false);
+        setMax(0);
+      }else{
+        setMax(maxLength);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array ensures the effect runs only once after initial render
+
 
   const handleClick = () => {
     setIsClicked(!isClicked);
@@ -16,9 +36,9 @@ function TruncatedText({ text, maxLength }: TruncatedTextProps): JSX.Element {
   return (
     <div onClick={handleClick} className="text-container" style={{ cursor: 'pointer' }}>
       {isClicked? (
-        <div className="truncated-text">{text}</div>
+        <div>{text}</div>
       ) : (
-        <div>{text.slice(0, maxLength)}{text.length > maxLength ? '...' : ''}</div>
+        <div>{text.slice(0, max)}{text.length > max ? '...' : ''}</div>
       )}
     </div>
   );
